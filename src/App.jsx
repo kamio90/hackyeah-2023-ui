29d0e2151aadcc6347016d0521f0e80e78b86998
kamio90 <kmusial40@gmail.com>
2023-09-30 16:30:58 +0200

    fix: clean up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.scss';
-import Form from './components/form/Form';
-import XmlToJsonViewer from './components/XmlToJsonViewer/XmlToJsonViewer';
-
+import "./App.scss";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      {/* <Form></Form> */}
-      <XmlToJsonViewer />
-      {/* <Form /> */}
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
