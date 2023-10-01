1a569203f0668fcfa5d3870f3e59054e5aa7ac9f
MarcinGa <marcin.gasiorek91@gmail.com>
2023-10-01 08:22:20 +0200

    au

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Zmienia domyślną nazwę pliku wyjściowego dla zachowania kompatybilności z niektórymi serwerami
+        entryFileNames: [name].js
+      }
+    },
+    // Jeśli hostujesz aplikację w podkatalogu, np. example.com/my-app/, ustaw wartość base na '/my-app/'
+    base: '/'
+  }
 })
