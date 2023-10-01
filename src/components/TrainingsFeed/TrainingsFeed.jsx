a5cf72d214a165ec18aafa3ff98003bc1df5df63
MarcinGa <marcin.gasiorek91@gmail.com>
2023-10-01 08:56:47 +0200

    Revert "poprawa T"
    This reverts commit e212d5c067b67e6a8a08116807a76e4ca0c0d356.

--- src/components/TrainingsFeed/TrainingsFeed.jsx
+++ src/components/TrainingsFeed/TrainingsFeed.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './TrainingsFeed.scss';
+import './trainingsFeed.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationPin} from '@fortawesome/free-solid-svg-icons';
 
